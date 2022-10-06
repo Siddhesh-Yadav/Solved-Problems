bbd9aa74d29b2ac02c770b4a224a7f7012ca2b83
Siddhesh Murlidhar Yadav <92988145+Siddhesh-Yadav@users.noreply.github.com>
2022-10-06 23:10:57 +0530

    added some more

--- Problems.cpp
+++ Problems.cpp
@@ -3,34 +3,29 @@ using namespace std;
 
 // Problem no 0 :- Print hello world!
 // int main(){
-// cout << "Hello World!\n";
-// return 0;
-// }
-
-
-// Problem no 1 :- Print a solid rectangle n(5)
-// int main(){
-//     int rows, columns;
-//     cout<<"rows "<<"columns";
-//     cin>>rows>>columns;
-//     for (int i = 1; i <= rows; i++){
-//         for (int j = 1; j <= columns; j++){
-//             cout<<"*";
-//         }
-//         cout<< endl;
-//     }
-    
+//     cout << "Hello World!\n";
 //     return 0;
 // }
 
 
+int main(){
+    int rows, columns;
+    cout<<"Enter number of Rows\n";
+    cin>>rows;
+    cout<<"Enter number of Columns\n";
+    // cin>>columns;
+
+
+// Problem no 1 :- Print a solid rectangle.
+    // for (int i = 1; i <= rows; i++){
+    //     for (int j = 1; j <= columns; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<< endl;
+    // }
+
+
 // Problem no 2 :- Print a hollow rectangle
-// int main(){
-//     int rows, columns;
-//     cout<<"Enter number of Rows";
-//     cin>>rows;
-//     cout<<"Enter number of Columns";
-//     cin>>columns;
 //     for (int i = 1; i <= rows; i++){
 //         for (int j = 1; j <= columns; j++){
 //             if(i==1 || i == rows || j==1 || j== columns){
@@ -40,21 +35,93 @@ using namespace std;
 //             }
 //         }
 //         cout<< endl;
-//     }    
-//     return 0;
-// }
+//     }
 
 
 // Problem no 3 :- Print a half pyramid
-// int main(){
-//     cout<<"Enter no of rows";
-//     int rows;
-//     cin>>rows;
 //     for(int i = 1; i<=rows;i++){
 //         for(int j = 1; j <= i; j++){
 //             cout<<"*";
 //         }
 //         cout<<endl;
 //     }
-//     return 0;
+
+// Problem no 4 :- Print a inverted half pyramid
+    // for(int i = rows; i >= 1; i--){
+    //     for(int j = 1; j <= i; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+// Problem no 5 :- Print a half pyramid 180 digree.
+// for(int i = 1; i <= rows; i++){
+//     for (int j = 1; j <= rows; j++){
+//         if(j<= rows-i){
+//             cout<<" ";
+//         }else{
+//             cout<<"*";
+//         }
+//     }
+//     cout<<endl;
 // }
+
+// Problem no 6 :- Print a half pyramid using numbers.
+// for(int i = 1; i <= rows; i++){
+//     for(int j = 1 ; j <= i; j++){
+//         cout<<j;
+//     }
+//     cout<<endl;
+// }
+
+// Problem no 7 :- Print a inverted half pyramid using numbers.
+// for(int i = rows; i >= 1; i--){
+//     for(int j = 1; j <= i; j++){
+//         cout<<j;
+//     }
+//     cout<<endl;
+// }
+
+// Problem no 8 :- Print half pyramid using numbers - 2.
+// for(int i = 1; i <= rows; i++){
+//     for(int j = 1; j <= i; j++){
+//         cout<<i;
+//     }
+//     cout<<endl;
+// }
+
+// Problem no 9 :- Print inverted half pyramid using numbers.
+// (numbers should be order of row)
+// for(int i = 1; i <= rows; i++){
+//     for(int j = 1; j <= rows - i + 1; j++){
+//         cout<<i;
+//     }
+//     cout<< endl;
+// }
+
+// Problem no 10 :- Print floyd's Triangle.
+// int a = 1;
+// for(int i = 1; i <= rows; i++){
+//     for(int j = 1; j <= i; j++){
+//         cout << a <<" ";
+//         a++;
+//     }
+//     cout<<endl;
+// }
+
+// Problem no 11 :- Print 0-1 triangle.
+int a = 1;
+for(int i = 1; i <= rows; i++){
+    for(int j = 1; j <= i; j++){
+        cout<< a <<" ";
+        a == 0?a = 1: a = 0;
+    }
+    cout<<endl;
+}
+
+    return 0;
+}
+
+
+
