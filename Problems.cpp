d3d9b9bd6fe972494662e44ae3736e4fbea7e2fc
Siddhesh Murlidhar Yadav <92988145+Siddhesh-Yadav@users.noreply.github.com>
2022-10-06 21:50:46 +0530

    cpp commit 3 problems

---
+++ Problems.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+// Problem no 0 :- Print hello world!
+// int main(){
+// cout << "Hello World!\n";
+// return 0;
+// }
+
+
+// Problem no 1 :- Print a solid rectangle n(5)
+// int main(){
+//     int rows, columns;
+//     cout<<"rows "<<"columns";
+//     cin>>rows>>columns;
+//     for (int i = 1; i <= rows; i++){
+//         for (int j = 1; j <= columns; j++){
+//             cout<<"*";
+//         }
+//         cout<< endl;
+//     }
+    
+//     return 0;
+// }
+
+
+// Problem no 2 :- Print a hollow rectangle
+// int main(){
+//     int rows, columns;
+//     cout<<"Enter number of Rows";
+//     cin>>rows;
+//     cout<<"Enter number of Columns";
+//     cin>>columns;
+//     for (int i = 1; i <= rows; i++){
+//         for (int j = 1; j <= columns; j++){
+//             if(i==1 || i == rows || j==1 || j== columns){
+//                 cout<<"*";
+//             }else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<< endl;
+//     }    
+//     return 0;
+// }
+
+
+// Problem no 3 :- Print a half pyramid
+// int main(){
+//     cout<<"Enter no of rows";
+//     int rows;
+//     cin>>rows;
+//     for(int i = 1; i<=rows;i++){
+//         for(int j = 1; j <= i; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
